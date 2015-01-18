d43de2670c0beb6f8fdca82048dd8665d39d61f1
João Vitor dos Santos honorato <www.jotave@gmail.com>
2015-01-18 17:07:33 -0300

    Some correction of Behavior of the player
    Fix animation, fix collider detection of wall, improve image quality of
    the animation

--- Project/Assets/Scripts/PlayerMoviment.cs
+++ Project/Assets/Scripts/PlayerMoviment.cs
@@ -4,8 +4,9 @@ using System.Collections.Generic;
 
 public class PlayerMoviment : MonoBehaviour {
 
-    Animator anim;    
-
+    Animator anim;
+    bool colliding = false;
+    
     public float speed = 10f;
 
 	void Start(){
@@ -18,70 +19,40 @@ public class PlayerMoviment : MonoBehaviour {
             Input.GetAxis("Vertical")
         );
 
-        Vector3 deltaMv = mv * speed * Time.deltaTime;
-        if (!CheckWallCollider(transform.position + deltaMv, mv)) {
-            var mainAxis = Mathf.Abs(Input.GetAxis("Horizontal")) > Mathf.Abs(Input.GetAxis("Vertical"));
-            var animSpeed = 0.75f * Mathf.Max(Mathf.Abs(Input.GetAxis("Horizontal")), Mathf.Abs(Input.GetAxis("Vertical")));
-            
-            print("moving on");
-
-            anim.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
-            anim.SetFloat("Vertical", Input.GetAxis("Vertical"));
-            anim.SetBool("MainAxis", mainAxis);
-            anim.speed = 0.25f + animSpeed;
-
-            transform.position = transform.position + deltaMv;
-        } else {
-            anim.SetFloat("Horizontal", 0);
-            anim.SetFloat("Vertical", 0);
-        }
-
-        /// Collider Test
-        
-        /*Vector2 v = ((Vector2) transform.position) + new Vector2(0, -0.7f);
-        Collider2D[] test = Physics2D.OverlapCircleAll(v, 0.4f);
-
-        print(test[0]);*/
+        Vector3 deltaMv = mv * speed;
+        rigidbody2D.velocity = deltaMv;
+        Animation();
     }
 
-    Vector2[] points = new Vector2[4];
-    Vector2[] /*deltaMatrix*/ dm = new Vector2[]{
-        Vector2.up, -Vector2.up, -Vector2.right, Vector2.right
-    };
-
-    public bool CheckWallCollider(Vector2 position, Vector2 moveVector) {
-        Collider2D[] /*colliderMatrix*/ cm = new Collider2D[4];
-        Vector2 center = position + new Vector2(0, -0.7f);
-
-        Vector2 point = center + (moveVector * 0.4f);
-        Collider2D collider = Physics2D.OverlapPoint(point, LayerMask.GetMask("Floor"));
-
-        if(collider != null)
-            return false;
-        return true;
-
-        /*for (int i = 0; i < 4; i++) {
-            points[i] = center + (dm[i] * 0.4f);
-            cm[i] = Physics2D.OverlapPoint(points[i], LayerMask.GetMask("Floor"));
-        }
-
-        if (System.Array.IndexOf(cm, null) == -1)
-            return false;
-        return true;
-        */
-
+    void Update() {
+        Animation();
     }
 
-    void OnDrawGizmos() {
-        Vector3 /*moveVector*/ mv = new Vector3(
-            Input.GetAxis("Horizontal"),
-            Input.GetAxis("Vertical")
-        );
+    void Animation() {
 
-        Gizmos.color = Color.yellow;
-        Vector3 v = transform.position + new Vector3(0, -0.7f) + (mv * 0.4f);
-        //Vector3 v = transform.position + new Vector3(0, -0.7f, 0);
-        //Gizmos.DrawWireSphere(v, 0.4f);
-        Gizmos.DrawWireSphere(v, 0.1f);
+        var mainAxis = Mathf.Abs(Input.GetAxis("Horizontal")) > Mathf.Abs(Input.GetAxis("Vertical"));
+        float vx = rigidbody2D.velocity.x / speed;
+        float vy = rigidbody2D.velocity.y / speed;
+
+
+        print(vx + ", " + vy);
+
+        float animSpeed = 0.9f * Mathf.Max(Mathf.Abs(vx), Mathf.Abs(vy)) + 0.1f;
+
+        anim.SetFloat("Horizontal", vx < -0.01f || vx > 0.01f ? Input.GetAxis("Horizontal") : 0);
+        anim.SetFloat("Vertical", vy != 0.0f ? Input.GetAxis("Vertical") : 0);
+
+        anim.SetBool("MainAxis", mainAxis);
+        anim.speed = colliding ? animSpeed : 1;
+    }
+
+    void OnCollisionEnter2D(Collision2D coll) {
+        print("Enter");
+        colliding = true;
+    }
+
+    void OnCollisionExit2D(Collision2D coll) {
+        print("Out");
+        colliding = false;
     }
 }
