02824d05f4d3585c8fb4f2062f50a9bb6cf630b3
Victor dos Santos <vitocr_santos@hotmail.com>
2015-02-27 15:55:12 -0300

    Refactoring the basic follow AI

---
+++ Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    public float life = 100f, damage = 2f, speed = 1f;
+    public Transform target = null;
+    protected Vector3 heading = Vector3.zero, direction = Vector3.zero;
+    protected float distance = 0f;
+
+    protected void UpdatePosition()
+    {
+        heading = target.position - this.gameObject.transform.position;
+        distance = heading.magnitude;
+        direction = heading / distance;	  
+    }
+
+    protected virtual void Defense() { }
+
+    protected virtual void Attack() { }
+
+    protected virtual void Movement() { }
+}
--- Project/Assets/Scripts/Follow.cs
+++ Project/Assets/Scripts/Follow.cs
@@ -1,44 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Follow : MonoBehaviour {
+public class Follow : Enemy {
 
-    public float speed;
-    public Transform target;
-    private Vector3 heading, direction;
-    private float distance;
     private bool seek;
 
 	// Use this for initialization
 	void Start () {
         seek = true;
+        target = GameObject.FindGameObjectWithTag("Player").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (seek)
         {
-            heading = target.position - this.gameObject.transform.position;
-            distance = heading.magnitude;
-            direction = heading / distance;
-
-            this.gameObject.transform.position += direction * speed * Time.deltaTime;
-
-            //if (heading.sqrMagnitude < 1)        
-            //print(distance + " | " + direction);
-
-            Debug.DrawLine(this.gameObject.transform.position, target.position);
+            UpdatePosition();
+            Moviment();
         }
 	}
+    
+    void Moviment()
+    {
+        this.gameObject.transform.position += direction * speed * Time.deltaTime;
+        Debug.DrawLine(this.gameObject.transform.position, target.position);
+    }
+
+    void Attack(GameObject target)
+    {
+        print(target.gameObject.name);
+        if (target.gameObject.tag == "Player")
+        {
+            Debug.LogWarning("Matei!");
+            //Object.Destroy(GameObject.Find("Beta (SideKick)"));
+            Object.Destroy(target.gameObject);
+            seek = false;
+        }
+    }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        print(collision.gameObject.name);
-        if (collision.gameObject.tag == "Player")
-        {
-            Debug.LogWarning("Matei!");
-            Object.Destroy(collision.gameObject);
-            seek = false;
-        }
+        Attack(collision.gameObject);       
     }
 }
