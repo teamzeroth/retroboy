63050000a4d5e0cddb4aa68bbcbad977d6f51a3a
Victor dos Santos <vitocr_santos@hotmail.com>
2015-04-10 23:14:14 -0300

    Stinger AI v0.1
    Similar to Machine Dog AI, with a few changes on Movement

--- Project2/Assets/Scripts/Enemy Behaivers/Range.cs
+++ Project2/Assets/Scripts/Enemy Behaivers/Range.cs
@@ -3,10 +3,12 @@ using System.Collections;
 
 public class Range : MonoBehaviour {
 
+    public Enemy m;
+
     void OnTriggerEnter2D(Collider2D trigger)
     {
         //print(this.gameObject.name);
-        MachineDog m = this.gameObject.transform.parent.gameObject.GetComponent<MachineDog>();        
+        //MachineDog m = this.gameObject.transform.parent.gameObject.GetComponent<MachineDog>();        
         m.onTriggerExternal(this.gameObject,trigger);
     }  
 }
