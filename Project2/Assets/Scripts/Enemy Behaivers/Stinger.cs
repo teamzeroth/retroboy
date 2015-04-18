3f98f53e072bea4bd0223d3a6202502f5f9cdbc0
m1thr4nd1r <vitocr_santos@hotmail.com>
2015-04-18 18:06:55 -0300

    Refactoring Stingers code

--- Project2/Assets/Scripts/Enemy Behaivers/Stinger.cs
+++ Project2/Assets/Scripts/Enemy Behaivers/Stinger.cs
@@ -15,7 +15,8 @@ public class Stinger : Enemy
     protected override void Start()
     {
         base.Start();
-        t = null;
+		speed = 10f;
+		t = null;
         movementDirection = 1f;
         count = 0;
         z = 0f;
@@ -38,45 +39,47 @@ public class Stinger : Enemy
     protected override void Movement()
     {
         Debug.DrawLine(this.gameObject.transform.position, target.transform.position - new Vector3(0f, 0.4f, 0f));
-            
-        if (seek)
+        
+		print(body.velocity + " | " + seek + " | " +  direction);	
+		body.AddForce(direction * speed, ForceMode2D.Impulse);
+		if (distance < 0.2 && seek)
         {
-            //lookAt2D(Vector3.forward);   
-            body.velocity = Vector2.right * movementDirection;
-            seek = false;
-            movementDirection *= -1f;
-            this.gameObject.transform.localScale = localScale;
-            StartCoroutine("changeSeek", 1f);
-            localScale.x *= -1;
+			body.AddForce(Vector2.up * -1, ForceMode2D.Impulse);
+//            seek = false;
+//            movementDirection *= -1f;
+//            this.gameObject.transform.localScale = localScale;
+//            StartCoroutine("changeSeek", 0.5f);
+//            localScale.x *= -1;
         }
-        else if (destroy)
-        {
-            if (this.transform.rotation.eulerAngles.z > maxRotation)
-            {
-                rotateStep *= -1f;
-                count++;
-            }
-
-            t = target.transform;
-            
-            Vector3 position = (target.transform.position - this.gameObject.transform.position - new Vector3(0f,0.4f,0f));
-            print(target.transform.position + " | " + position + " | " + position.magnitude + " | " + position / position.magnitude + " | " + (position / position.magnitude).magnitude);
-            this.gameObject.transform.RotateAround(t.position - new Vector3(0f,0.4f,0f), Vector3.forward,1 * rotateStep);
-//            lookAt2D(t.position);            
-
-            if (count == 2)
-            {
-                StopAllCoroutines();
-                count = 0;
-                destroy = false;
-                seek = true;
-            }
-        }
+//        else if (destroy)
+//        {
+//            if (this.transform.rotation.eulerAngles.z > maxRotation)
+//            {
+//                rotateStep *= -1f;
+//                count++;
+//            }
+//
+//            t = target.transform;
+//            
+//            Vector3 position = (target.transform.position - this.gameObject.transform.position - new Vector3(0f,0.4f,0f));
+//            print(target.transform.position + " | " + position + " | " + position.magnitude + " | " + position / position.magnitude + " | " + (position / position.magnitude).magnitude);
+//            this.gameObject.transform.RotateAround(t.position - new Vector3(0f,0.4f,0f), Vector3.forward,1 * rotateStep);
+////            lookAt2D(t.position);            
+//
+//            if (count == 2)
+//            {
+//                StopAllCoroutines();
+//                count = 0;
+//                destroy = false;
+//                seek = true;
+//            }
+//        }
     }
 
     protected override void Attack(GameObject obj)
     {
-        if (obj != null && destroy)
+		if (false)
+		//if (obj != null && destroy)
         {
             z = this.gameObject.transform.rotation.eulerAngles.z;
             bullet = prefab.Spawn();
